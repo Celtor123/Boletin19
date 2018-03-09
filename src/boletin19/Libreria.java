484c23c275818f54a95f34d59fc37fc641735cc5
csampedroromero <csampedroromero@victoria25.danielcastelao.org>
2018-03-09 12:55:32 +0100

    Inicio a.

--- src/boletin19/Libreria.java
+++ src/boletin19/Libreria.java
@@ -1,15 +1,38 @@
 
 package boletin19;
-
 import java.util.ArrayList;
-
+import javax.swing.JOptionPane;
 public class Libreria {
-    
-    public void agregar(ArrayList <Libro> lista){
-      
-        lista.add(new Libro("a"+"w"+"w"+2+3.3));
-          System.out.println(lista);
-       
+    Libro k=new Libro();
+        ArrayList<Libro> lista = new ArrayList<>();
+
+    public Libreria() {
+    }
+        
+    public void agregar(){
+        String w=JOptionPane.showInputDialog("Ingrese nome");
+         String y=JOptionPane.showInputDialog("Ingrese autor");
+          String t=JOptionPane.showInputDialog("Ingrese ISBN");
+          String e=JOptionPane.showInputDialog("Ingrese numero de unidad");
+         int s= Integer.parseInt(e);
+             String i=JOptionPane.showInputDialog("Ingrese prezo:");
+         int a= Integer.parseInt(i);
+        lista.add(new Libro(w,y,t,s,a));
+         
+    }
+    public void venderLibros(int a){
+        lista.remove(a);
+    }
+    public void amosarLibros(){
+        
+    System.out.println(lista);
+    }
+    public void darBaixa(){
+        for(){
+        if(lista.contains(titulo=null)){
+            lista.remove)
+        }
+        }
     }
     
        
