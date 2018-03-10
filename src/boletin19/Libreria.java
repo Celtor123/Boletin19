597654023b6b284383a0ebefb2c184907448cff6
Juan <Juan@DESKTOP-J5PU6RU>
2018-03-10 19:48:12 +0100

    Listo Boletin .

--- src/boletin19/Libreria.java
+++ src/boletin19/Libreria.java
@@ -1,12 +1,22 @@
 
 package boletin19;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import javax.swing.JOptionPane;
-public class Libreria {
-    Libro k=new Libro();
+public class Libreria{
+ 
         ArrayList<Libro> lista = new ArrayList<>();
 
     public Libreria() {
+    }
+
+    public ArrayList<Libro> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Libro> lista) {
+        this.lista = lista;
     }
         
     public void agregar(){
@@ -16,26 +26,52 @@ public class Libreria {
           String e=JOptionPane.showInputDialog("Ingrese numero de unidad");
          int s= Integer.parseInt(e);
              String i=JOptionPane.showInputDialog("Ingrese prezo:");
-         int a= Integer.parseInt(i);
+         float a= Float.parseFloat(i);
         lista.add(new Libro(w,y,t,s,a));
          
     }
-    public void venderLibros(int a){
-        lista.remove(a);
+    public void venderLibros(){
+        String w=JOptionPane.showInputDialog("Poña o numero do libro que quere eliminar");
+        int r=Integer.parseInt(w);
+        lista.clear();
     }
     public void amosarLibros(){
-        
-    System.out.println(lista);
+        Collections.sort(lista);
+        for(Libro a:lista){
+    System.out.println(a);
+        }
     }
     public void darBaixa(){
-//        for(){
-//        if(lista.contains(titulo=null)){
-//            lista.remove)
-//        }
-//        }
+    for(Libro noUnidade:lista){
+        if(noUnidade!=null){
+           
+        }
+        else{
+             lista.remove(noUnidade);
+        }
     }
-    
+        
+    }
+       public void consultarLibro(){
+          String zs=JOptionPane.showInputDialog("Meta o nome do libro");
+            for (Libro titulo : lista) {
+                System.out.println(titulo);
+                if(titulo.getTitulo()==zs){
+                    System.out.println(titulo);
+                }
+                else{
+                     System.out.println("No existe el libro");
+                }
+                          }
+           
+           }
+       
        
     
+
+    @Override
+    public String toString() {
+        return "Libreria{" + "lista=" + lista + '}';
+    }
     
 }
